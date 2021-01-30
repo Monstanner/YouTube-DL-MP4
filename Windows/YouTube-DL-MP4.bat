b861e27cea0bf405404919f9ec5bf0c279d89c49
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-01-30 14:15:24 +0100

    Update YouTube-DL-MP4.bat

--- Windows/YouTube-DL-MP4.bat
+++ Windows/YouTube-DL-MP4.bat
@@ -1,1 +1,7 @@
-
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 137+140 --merge-output-format -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
