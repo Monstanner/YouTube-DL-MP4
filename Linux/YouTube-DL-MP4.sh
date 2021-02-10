88949f099370955dc7adaac0443df71ffcc64388
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-02-10 17:30:39 +0100

    Update YouTube-DL-MP4.sh

--- Linux/YouTube-DL-MP4.sh
+++ Linux/YouTube-DL-MP4.sh
@@ -3,7 +3,7 @@
 cd #Hier bitte den Pfad eingeben, wo das Video gespeichert werden soll.
 
 echo ---------------------------------------------------
-echo Bitte füge den Link zu dem Video ein (Strg / Ctrl + Shift + V oder Rechtsklick > Einfügen) und bestätige dann mit Enter.
+echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick, dann Einfügen und bestätige dann mit Enter.
 echo ---------------------------------------------------
 while read INPUT
  do
