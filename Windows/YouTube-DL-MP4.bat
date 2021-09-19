8a0b690d2a9577a5d340ee5616c86f8b2d6688dd
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-09-19 01:10:44 +0000

    Update YouTube-DL MP4.bat
    YouTube-DL MP4 v. 1.3:
    
    Auswahl zwischen Videoauflösungen hizugefügt:
    
    - 144p
    - 240p
    - 360p
    - 480p
    - 720p
    - 720p60fps
    - 1080p
    - 1080p60fps
    - 1440p
    - 1440p60fps
    - 2160p
    - 2160p60fps
    - Beste Qualität (Video+Audio) (Fuer andere Plattformen)
    
    Sektionen für Über und Beenden hinzugefügt:
    
    - Über (Zeigt Infos zu den Skriot an)
    - Beenden (Beendet das Skript)

--- Windows/YouTube-DL-MP4.bat
+++ Windows/YouTube-DL-MP4.bat
@@ -1,7 +1,160 @@
+@ECHO off
+cls
+:start
+ECHO.
+ECHO ---------------------------------------------------
+ECHO YouTube-DL MP4 v.1.3 20210919 by Monstanner
+ECHO ---------------------------------------------------
+ECHO 1. 144p (Low-Definition)
+ECHO 2. 240p (Low-Definition)
+ECHO 3. 360p (Low-Definition)
+ECHO 4. 480p (Standard-Definition)
+ECHO 5. 720p (High-Definition)
+ECHO 6. 720p60fps (High-Definition + 60fps)
+ECHO 7. 1080p (Full-High-Definition)
+ECHO 8. 1080p60fps (Full-High-Definition + 60fps)
+ECHO 9. 1440p (Quad-High-Definition)
+ECHO A. 1440p60fps (Quad-High-Definition + 60fps)
+ECHO B. 2160p (Ultra-High-Definition 4K)
+ECHO C. 2160p60fps (Ultra-High-Definition 4K + 60fps)
+ECHO D. Beste Qualitaet (Video+Audio) (Fuer andere Plattformen)
+ECHO E. Ueber
+ECHO F. Beenden
+
+set choice=
+set /p choice=In welcher Qualitaet soll das Video heruntergeladen werden? (Das Video sollte in der Qualitaet verfuegbar sein).
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto 1
+if '%choice%'=='2' goto 2
+if '%choice%'=='3' goto 3
+if '%choice%'=='4' goto 4
+if '%choice%'=='5' goto 5
+if '%choice%'=='6' goto 6
+if '%choice%'=='7' goto 7
+if '%choice%'=='8' goto 8
+if '%choice%'=='9' goto 9
+if '%choice%'=='A' goto 10
+if '%choice%'=='B' goto 11
+if '%choice%'=='C' goto 12
+if '%choice%'=='D' goto 13
+if '%choice%'=='E' goto 14
+if '%choice%'=='F' goto 15
+ECHO "%choice%" ist keine bekannte Option. Bitte eine Zahl zwischen 1 bis 9 oder einen Buchstaben von A bis F eingeben.
+ECHO.
+goto start
+:1
 @ECHO OFF
 SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
 ECHO.
-youtube-dl -f 137+140 --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+youtube-dl -f 160+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
 ECHO.
 PAUSE
 EXIT
+:2
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 133+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:3
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 134+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:4
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 135+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:5
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 136+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:6
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 136+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:7
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 137+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:8
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 299+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:9
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 400+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:10
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 400+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:11
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 401+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:12
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f 401+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:13
+@ECHO OFF
+SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
+ECHO.
+youtube-dl -f bestvideo+bestaudio --merge-output-format mp4 -o "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+PAUSE
+EXIT
+:14
+ECHO ---------------------------------------------------
+ECHO YouTube-DL MP4 ist ein Terminal Script und wurde von Monstanner erstellt.
+ECHO YouTube-DL gibt es fuer GNU/Linux und Windows.
+ECHO Aktuelle Version: 1.3 20210919
+ECHO Link zu YouTube-DL MP4: www.github.com/monstanner
+ECHO ---------------------------------------------------
+goto start
+:15
+goto end
+:end
+pause
