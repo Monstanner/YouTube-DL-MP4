5d783c4e717688bbe5e3cdb58521447e5e52b9a6
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-09-19 01:15:56 +0000

    Update YouTube-DL-MP4.sh
    YouTube-DL MP4 v. 1.3:
    
    Auswahl zwischen Videoauflösungen hizugefügt:
    
    - Beste Qualität (Video+Audio) (Für andere Plattformen
    
    Sektionen für Über und Beenden hinzugefügt:
    
    - Über (Zeigt Infos zu den Skriot an)
    - Beenden (Beendet das Skript)
    
    Sonstiges:
    
    - Rechtschreibfehler behoben

--- Linux/YouTube-DL-MP4.sh
+++ Linux/YouTube-DL-MP4.sh
@@ -1,163 +1,186 @@
 #!/bin/bash
 
 echo ---------------------------------------------------
-echo YouTube-DL MP4 v.1.2 20210919 by Monstanner
+echo YouTube-DL MP4 v.1.3 20210919 by Monstanner
 echo ---------------------------------------------------
 
 cd #Hier bitte den Pfad eingeben, wo das Video gespeichert werden soll.
 
 #!/bin/bash
 
-PS3='In welches Qualität soll das Video heruntergeladen werden? (Das Video sollte in der Qualität verfügbar sein) '
-options=("144p" "240p" "360p" "480p" "720p" "720p60fps" "1080p" "1080p60fps" "1440p" "1440p60fps" "2160p" "2160p60fps")
+PS3='In welcher Qualität soll das Video heruntergeladen werden? (Das Video sollte in der Qualität verfügbar sein). '
+options=("144p (Low-Definition)" "240p (Low-Definition)" "360p (Low-Definition)" "480p (Standard-Definition)" "720p (High-Definition)" "720p60fps (High-Definition + 60fps)" "1080p (Full-High-Definition)" "1080p60fps (Full-High-Definition + 60fps)" "1440p (Quad-High-Definition)" "1440p60fps (Quad-High-Definition + 60fps)" "2160p (Ultra-High-Definition 4K)" "2160p60fps (Ultra-High-Definition 4K + 60fps)" "Beste Qualität (Video+Audio) (Für andere Plattformen)" "Über" "Beenden")
 select opt in "${options[@]}"
 do
   case $opt in
-          "144p")
+          "144p (Low-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 160+249 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 160+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "240p")
+          "240p (Low-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 133+249 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 133+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "360p")
+          "360p (Low-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 134+249 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 134+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "480p")
+          "480p (Standard-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 135+250 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 135+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "720p")
+          "720p (High-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 136+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 136+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "720p60fps")
+          "720p60fps (High-Definition + 60fps)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 136+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 136+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "1080p")
+          "1080p (Full-High-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 137+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 137+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "1080p60fps")
+          "1080p60fps (Full-High-Definition + 60fps)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 299+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 299+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "1440p")
+          "1440p (Quad-High-Definition)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 400+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 400+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "1440p60fps")
+          "1440p60fps (Quad-High-Definition + 60fps)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 400+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 400+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "2160p")
+          "2160p (Ultra-High-Definition 4K)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 401+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 401+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-          "2160p60fps")
+          "2160p60fps (Ultra-High-Definition 4K + 60fps)")
               echo ---------------------------------------------------
               echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
               echo ---------------------------------------------------
               while read INPUT
               do
                 echo Wird heruntergeladen...
-                youtube-dl -f 401+251 --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                youtube-dl -f 401+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
                 break
               done
               exit
               ;;
-       *) echo Keine bekannte Option. Bitte wähle eine Zahl zwischen 1 bis 12 aus.;;
+          "Beste Qualität (Video+Audio) (Für andere Plattformen)")
+              echo ---------------------------------------------------
+              echo Bitte füge den Link zu dem Video ein Strg / Ctrl + Shift + V oder Rechtsklick + Einfügen und bestätige dann mit Enter.
+              echo ---------------------------------------------------
+              while read INPUT
+              do
+                echo Wird heruntergeladen...
+                youtube-dl -f bestvideo+bestaudio --merge-output-format mp4 -o "%(title)s.%(ext)s" "$INPUT"
+                break
+              done
+              exit
+              ;;
+          "Über")
+              echo ---------------------------------------------------
+              echo YouTube-DL MP4 ist ein Terminal Script und wurde von Monstanner erstellt.
+              echo YouTube-DL gibt es für GNU/Linux und Windows.
+              echo Aktuelle Version: 1.3 20210919
+              echo Link zu YouTube-DL MP4: www.github.com/monstanner
+              echo ---------------------------------------------------
+              ;;
+          "Beenden")
+              exit
+              ;;
+       *) echo Keine bekannte Option. Bitte wähle eine Zahl zwischen 1 bis 15 aus.;;
   esac
 done
 exit
